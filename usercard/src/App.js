3941e698120148a1d532686946f546f3d40d20c0
unknown <neduvinci@gmail.com>
2019-10-01 13:04:10 +0100

    Got data from github api and set to state

--- usercard/src/App.js
+++ usercard/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userDetails:{},
+    }
+  }
+
+  componentDidMount() {
+    axios.get(`https://api.github.com/users/rodpa715`)
+      .then(response => {
+        
+        debugger
+        this.setState({userDetails:response.data})
+    })
+  }
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">        
+        {this.state.userDetails.login}
+        <img src={this.state.userDetails.avatar_url} alt=""/>     
       </header>
     </div>
-  );
+    );
+  }
 }
-
-export default App;
